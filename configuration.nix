5b6fcf383594038746fa7a38e491b3f23ea3776a
birdmanmandbir <bn0010100@gmail.com>
2023-10-03 16:04:24 +0000

    init

---
+++ configuration.nix
@@ -0,0 +1,38 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, helix, ... }:
+
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "neil";
+  nix.settings.trusted-users = [ "neil" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = with pkgs; [
+    git  # Nix Flakes 通过 git 命令从数据源拉取依赖，所以必须先安装好 git
+    vim
+    wget
+    curl
+    # 这里从 helix 这个 inputs 数据源安装了 helix 程序
+    helix.packages."${pkgs.system}".helix
+  ];
+  # 将默认编辑器设置为 vim
+  environment.variables.EDITOR = "hx";  
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
